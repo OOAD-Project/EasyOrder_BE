56da1d61b0703c45cda4b133553e933141e8a5b0
yanglikun <2865767151@qq.com>
2018-06-30 17:48:36 +0800

    modify shop path and password

--- views/shop_view.py
+++ views/shop_view.py
@@ -3,7 +3,7 @@ import json
 
 async def get_shop(request):
     print("========================");
-    with open("./shop.json", 'r') as load_f:
+    with open("/var/www/aio_ooad/shop.json", 'r') as load_f:
         load_dict = json.load(load_f)
         print(load_dict)
         return web.json_response(load_dict)
