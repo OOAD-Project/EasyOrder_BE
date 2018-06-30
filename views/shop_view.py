47732bb9f12743d1b45ed4a6c848d116ba92edc0
gzm1997 <1617899539@qq.com>
2018-06-30 18:00:52 +0800

    Revert "change food id to str"
    This reverts commit 9138b7fbe995cec2c7a867adcf5b8ca68cf1e7ff.

--- views/shop_view.py
+++ views/shop_view.py
@@ -3,7 +3,7 @@ import json
 
 async def get_shop(request):
     print("========================");
-    with open("/home/gzm/three/系统分析与设计/大作业/EasyOrder_BE/shop.json", 'r') as load_f:
+    with open("./shop.json", 'r') as load_f:
         load_dict = json.load(load_f)
         print(load_dict)
         return web.json_response(load_dict)
